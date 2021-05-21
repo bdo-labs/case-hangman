5b97b12658bde3204439299e20d9694e4a07b88f
Joachim Ovesen <joachim.ovesen@bdo.no>
2021-05-21 10:13:38 +0200

    Client

--- HangmanAPI/HangmanClient/Program.cs
+++ HangmanAPI/HangmanClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HangmanAPI;
+using System;
 
 namespace HangmanClient
 {
@@ -10,10 +11,77 @@ namespace HangmanClient
             
             var solution = hangman.Play();
 
-            // input loop for client
-            // loop through characters from console
+            Print(new string(solution));
 
-            var guessResult = hangman.Guess('a');
+            while (true)
+            {
+                try
+                {
+                    var toGuess = GetInput();
+
+                    var result = hangman.Guess(toGuess);
+
+                    solution = result.Solution;
+                    switch(result.Status)
+                    {
+                        case Status.InProgress:
+                            var message = result.SuccessfulGuess ? "Correct!" : "Wrong :/";
+                            Print(new string(solution), message);
+                            break;
+                        case Status.Lost:
+                            PrintLost();
+                            break;
+                        case Status.Won:
+                            PrintWon(new string(solution));
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Print(new string(solution), ex.Message);
+                }
+            }
+        }
+
+        static char GetInput()
+        {
+            Console.WriteLine();
+            Console.Write("Your guess: ");
+            var input = Console.ReadLine();
+
+            if (input.Length > 1)
+            {
+                throw new Exception("Only one character allowed per entry");
+            }
+
+            return input[0];
+        }
+
+        static void Print(string state, string message = null)
+        {
+            Console.Clear();
+            Console.WriteLine("----- Hangman ----- ");
+            Console.WriteLine(state);
+            
+            if (message != null)
+            {
+                Console.WriteLine();
+                Console.WriteLine(message);
+            }
+        }
+
+        static void PrintLost()
+        {
+            Console.Clear();
+            Console.WriteLine("----- Hangman ----- ");
+            Console.WriteLine("You lost :(");
+        }
+
+        static void PrintWon(string state)
+        {
+            Console.Clear();
+            Console.WriteLine("----- Hangman ----- ");
+            Console.WriteLine("You won! The word was " + state);
         }
     }
 }
